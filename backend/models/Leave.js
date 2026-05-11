bb3118a79b569153b06b14d94c9336471abf9f49
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-05-11 13:49:50 +0530

    leave model optimized

--- backend/models/Leave.js
+++ backend/models/Leave.js
@@ -2,30 +2,33 @@ import mongoose from "mongoose";
 
 const leaveSchema = new mongoose.Schema({
     employee: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true,},
-    leaveType: {type: String,enum: ["Casual", "Earned", "Sick", "Weekoff"],required: true,},
+    leaveType: {type: String, enum: ["Casual", "Earned", "Sick", "Weekoff"], required: true,},
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     totalDays: { type: Number },
-    reason: { type: String, required: true, trim: true, maxlength: 500 },
-    status: {type: String,enum: ["pending", "approved", "rejected", "cancelled"],default: "pending",},
+    reason: { type: String, required: true, trim: true, minlength : 5, maxlength: 500 },
+    status: {type: String,enum: ["pending", "approved", "rejected", "cancelled"], default: "pending",},
     approvedBy: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
     approvedComment: { type: String, trim: true },
     attachment: { type: String },
     leaveBalanceSnapshot: { type: Number },
 },{ timestamps: true });
 
+//date validation
 leaveSchema.pre('save', function(next){
-  if(this.endDate < this.startDate){  //date validation
+  if(this.endDate < this.startDate){  
     return next(new Error('end date cannot be before start date'))
   }
 
-  if(this.startDate && this.endDate) { // calculate total days
+  // calculate total days
+  if(this.startDate && this.endDate) { 
     const diff = this.endDate - this.startDate
     this.totalDays = diff / (1000 * 60 * 60 * 24) + 1
   }
+  next()
 })
 
-leaveSchema.index({employee : 1, status : 1})
+leaveSchema.index({employee : 1, status : 1, startDate: 1, endDate : 1})
 
 const Leave = mongoose.model("Leave", leaveSchema);
 export default Leave;
