7e406bc48630a67344e6dd1b1eee8631a65e2efb
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-05-12 15:38:23 +0530

    backend: routes consistency improved

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -6,9 +6,9 @@ const taskRouter = express.Router()
 
 taskRouter.post('/create-task', authMiddleware, createTask)
 taskRouter.get('/',authMiddleware, getAllTasks)
-taskRouter.get('/:id',authMiddleware, getTask)
 taskRouter.put('/update-task/:id', authMiddleware, updateTask)
-taskRouter.put('/update-status/:id/status', authMiddleware, updateStatus)
+taskRouter.put('/update-status/:id', authMiddleware, updateStatus)
 taskRouter.delete('/delete-task/:id', authMiddleware, deleteTask)
+taskRouter.get('/:id',authMiddleware, getTask)
 
 export default taskRouter
